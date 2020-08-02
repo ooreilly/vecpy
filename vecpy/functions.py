6561c57f456902823b4fd49d0c62840bb825ee1b
Ossian O'Reilly <ossian.oreilly@gmail.com>
2020-08-01 19:16:28 -0700

    Test functions.

--- vecpy/functions.py
+++ vecpy/functions.py
@@ -1,5 +1,22 @@
 import vecpy as vp
 
+"""
+
+TODO: Implement functions for all CUDA Math library functions 
+https://docs.nvidia.com/cuda/cuda-math-api/ 
+
+A temporary work-around is to use `function` to create a custom function that matches the name of
+the function you want to use.
+
+Example: 
+
+`
+>>> erf = lambda expr : function("erf", expr)
+`
+to define the error function.
+
+"""
+
 
 class Function(vp.Expr):
 
@@ -14,6 +31,9 @@ class Function(vp.Expr):
         else:
             args_str = ", " +  ", ".join(["%s" % arg for arg in self.args])
         return "%s(%s%s)" % (self.label, self.expr.eval(code, i), args_str) 
+
+def function(name, expr, *args):
+    return Function(name, expr, *args)
 
 def sqrt(expr):
     return Function("sqrt", expr)
@@ -30,9 +50,12 @@ def exp(expr):
 def log(expr):
     return Function("log", expr)
 
+def abs(expr):
+    return Function("fabs", expr)
+
 def pow(expr, exp):
     return Function("pow", expr, exp)
 
-def function(name, expr, *args):
-    return Function(name, expr, args)
+def floor(expr):
+    return Function("floor", expr)
 
---
+++ vecpy/test/test_functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+import vecpy as vp
+
+a = np.random.randn(100).astype(np.float32)
+va = vp.Array(a, None, "a")
+
+def test_functions():
+    assert str(vp.function("test", va, 2)) == "test(a, 2)"
+    assert str(vp.sqrt(va)) == "sqrt(a)"
+    assert str(vp.sin(va)) == "sin(a)"
+    assert str(vp.cos(va)) == "cos(a)"
+    assert str(vp.exp(va)) == "exp(a)"
+    assert str(vp.log(va)) == "log(a)"
+    assert str(vp.abs(va)) == "fabs(a)"
+    assert str(vp.pow(va, 2)) == "pow(a, 2)"
+    assert str(vp.floor(va)) == "floor(a)"
