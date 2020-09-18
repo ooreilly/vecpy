44654f837226502297be4f10d45c389b7eaaaf3a
Ossian O'Reilly <ossian.oreilly@gmail.com>
2020-09-18 11:32:55 -0700

    update docs script.

--- docs.sh
+++ docs.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-root=../vecpy
-for func in elementwise sum
+root=vecpy/api
+for file in ${root}/*.py
 do
-        mydocstring ${root}/api/$func.py $func -m -T=template.md > docs/$func.md
+        file_name=$(basename $file)
+        function_name=${file_name%.py}
+
+        if [[ ${function_name} == "__init__" ]]
+        then
+                continue
+        fi
+
+        echo "Generating docs for ${function_name}"
+        mydocstring $file $function_name -m -T=template.md > docs/api/$function_name.md
 done
