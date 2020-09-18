2ce0ad71b5f380708d9431b7124d691fc12949c7
Ossian O'Reilly <ossian.oreilly@gmail.com>
2020-09-18 11:25:49 -0700

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vecpy',
-      version='0.1',
+      version='0.1.1',
       description='Light-weight library for vectorized operations on the GPU',
       author="Ossian O'Reilly",
       author_email='ossian.oreilly@gmail.com',
